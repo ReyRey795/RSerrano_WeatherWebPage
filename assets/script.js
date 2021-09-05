de5db74563fcc5875fb93c84da679c7eb1c35fdb
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-05 12:03:46 -0500

    Created a text area to put the city location, and a submit button. Additionally removed the api key becuase of it being a secret leak. Began styling in the CSS as well.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var APIkey = "aa46b7171a4a8a6c05d7c89ff0330127"
+var APIkey = ''
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 fetch(queryURL);
