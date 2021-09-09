f6c8bf05d6e56602f8af3c22949a4d6495744f85
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-09 16:44:08 -0500

    Added several variables to connect to their corresponding innerHTML values.

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,8 @@ var button = document.querySelector('.button');
 var inputValue = document.querySelector('.inputValue');
 var Cname = document.querySelector('.name');
 var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+var tempMin = document.querySelector('.tempMin');
+var tempMax = document.querySelector('.tempMax');
 var date = document.querySelector('.date');
 var lat = document.querySelector('.lat');
 var long = document.querySelector('.long');
@@ -13,20 +14,22 @@ var icon = document.querySelector('.icon');
 var humidity = document.querySelector('.humidity');
 var windSpeed = document.querySelector('.windSpeed');
 var uvIndex = document.querySelector('.uvIndex');
+var arrayDay = document.querySelector('.numberedDay')
 
 
 button.addEventListener('click', function() {
     fetch('http://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=imperial&appid=aa46b7171a4a8a6c05d7c89ff0330127')
     .then(response => response.json())
     .then(data => {
-        // console.log(data)
+        console.log(data)
         // console.log(data.city.name)
         // console.log(data.city.coord.lon)    
      var latitude = data.city.coord.lat;
      var longitude = data.city.coord.lon;
+
      lat.innerHTML = latitude;
      long.innerHTML = longitude;
-    
+     var nameValue = data.city.name;
    
 
     fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&units=imperial&appid=aa46b7171a4a8a6c05d7c89ff0330127')
@@ -37,9 +40,18 @@ button.addEventListener('click', function() {
 
          for (let i = 0; i < 5; i++){
              var dailyWeather = daily[i]
-             
-             var nameValue = data.coord.name;
 
+             
+             var arrayDayValue = dailyWeather;
+             var minTempValue = 'Min temp. '+dailyWeather.temp.min + '*F';
+             var maxTempValue = 'Max temp. '+dailyWeather.temp.max + '*F';
+             var descValue = dailyWeather.weather[0].description;
+             var iconValue = dailyWeather.weather[0].icon;
+             icon.innerHTML = iconValue;
+             desc.innerHTML = descValue;
+             tempMin.innerHTML = minTempValue;
+             tempMax.innerHTML = maxTempValue;
+             arrayDay.innerHTML = arrayDayValue;
              Cname.innerHTML = nameValue;
          
 
