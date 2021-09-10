b53300f41b1d8b72eb6bff093dfd5cb13bc36d20
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-09-09 22:44:47 -0500

    Created a collapsible function in order to hide and show the search history from user input. Added some hover attributes to the submit button as well.

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,20 @@ var windSpeed = document.querySelector('.windSpeed');
 var uvIndex = document.querySelector('.uvIndex');
 var arrayDay = document.querySelector('.numberedDay')
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 
 button.addEventListener('click', function() {
